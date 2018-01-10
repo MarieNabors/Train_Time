7af49440e79d826315383d17a8d15f3eb5923482
Marie Nabors <marienabors@thefirstacademy.org>
2018-01-10 14:16:24 -0500

    update

---
+++ trains.js
@@ -0,0 +1,13 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBouq6hyqZpc7ManlIFfscHyDAxKGlcgBM",
+    authDomain: "time-for-trains.firebaseapp.com",
+    databaseURL: "https://time-for-trains.firebaseio.com",
+    projectId: "time-for-trains",
+    storageBucket: "time-for-trains.appspot.com",
+    messagingSenderId: "303628179740"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database
