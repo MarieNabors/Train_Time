e0d8b07eba4cdf37105833e039e6021cb14744be
Marie Nabors <marienabors@thefirstacademy.org>
2018-01-10 21:51:32 -0500

    updating

--- trains.js
+++ trains.js
@@ -5,55 +5,52 @@ $("#submit").on("click", function(event) {
 
 //creating column element for new train name
   var trainName = $("<td>");
-
 //grab new train name text input 
   var text=  $("#trainName").val()
-
 //add text (new train name) to column element
   trainName.html(text)
+  //appending column to new row
+newTrain.append(trainName);
 
-//--------------//
 
 //creating column element for new train's destination
 var newPlace = $("<td>");
-
 //grab new place text input
 var place =$("newPlace");
-
 //add place to column element
 newPlace.html(place)
+//appending column to new row
+newTrain.append(newPlace);
 
 //----------------//
 
 //create new column element for departure time
 var newDeparture = $("<td>");
-
 //grab new time text input
 var leaveTime = $("newDeparture");
-
 //add new departure time to column element
 newDeparture.html(leaveTime)
+//appending column to new row
+newTrain.append(newDeparture);
 
 //----------------//
 
 var newArrival = $("<td>");
 
-//appending column to new row
-newTrain.append(trainName);
+
 
 //appending row to page
 $("#newRow").append(newTrain);
 
-var addTrain = {
+var newTrain = {
   name: text,
-  // destination: place,
-  // departure: leaveTime,
+  destination: place,
+  departure: leaveTime,
   // arrival: time,
 }
  var database = firebase.database()
 
   database.ref().push(addTrain);
-var tFrequency = 3;
 });
     // // Time is 3:30 AM
     // var firstTime = "03:30";
