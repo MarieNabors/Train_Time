4207f168449576dbe623ffcce122f608dfbf13e1
Marie Nabors <marienabors@thefirstacademy.org>
2018-01-10 21:37:48 -0500

    in progress

--- trains.js
+++ trains.js
@@ -1,34 +1,93 @@
+
+
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+//creating column element for new train name
+  var trainName = $("<td>");
+
+//grab new train name text input 
+  var text=  $("#trainName").val()
+
+//add text (new train name) to column element
+  trainName.html(text)
+
+//--------------//
+
+//creating column element for new train's destination
+var newPlace = $("<td>");
+
+//grab new place text input
+var place =$("newPlace");
+
+//add place to column element
+newPlace.html(place)
+
+//----------------//
+
+//create new column element for departure time
+var newDeparture = $("<td>");
+
+//grab new time text input
+var leaveTime = $("newDeparture");
+
+//add new departure time to column element
+newDeparture.html(leaveTime)
+
+//----------------//
+
+var newArrival = $("<td>");
+
+//appending column to new row
+newTrain.append(trainName);
+
+//appending row to page
+$("#newRow").append(newTrain);
+
+var addTrain = {
+  name: text,
+  // destination: place,
+  // departure: leaveTime,
+  // arrival: time,
+}
+ var database = firebase.database()
+
+  database.ref().push(addTrain);
 var tFrequency = 3;
+});
+    // // Time is 3:30 AM
+    // var firstTime = "03:30";
 
-    // Time is 3:30 AM
-    var firstTime = "03:30";
+    // // First Time (pushed back 1 year to make sure it comes before current time)
+    // var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "minutes");
+    // console.log(firstTimeConverted);
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "minutes");
-    console.log(firstTimeConverted);
+    // // Current Time
+    // var currentTime = moment();
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // // Difference between the times
+    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
+    // // Time apart (remainder)
+    // var tRemainder = diffTime % tFrequency;
+    // console.log(tRemainder);
 
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
+    // // Minute Until Train
+    // var tMinutesTillTrain = tFrequency - tRemainder;
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // // Next Train
+    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
   
 
 //need code that updates the train list and submits the information to firebase //
+// Uploads train info to the database
+ // var database = firebase.database()
+
+  //database.ref().push(addTrain);
 
 //you need to write the code that gets the train records from firebase
 
