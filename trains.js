1fcc6ae97da479d14828f5b9a3f6a23fb82fb2e5
Marie Nabors <marienabors@thefirstacademy.org>
2018-01-10 19:58:45 -0500

    update

--- trains.js
+++ trains.js
@@ -1,13 +1,37 @@
+var tFrequency = 3;
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBouq6hyqZpc7ManlIFfscHyDAxKGlcgBM",
-    authDomain: "time-for-trains.firebaseapp.com",
-    databaseURL: "https://time-for-trains.firebaseio.com",
-    projectId: "time-for-trains",
-    storageBucket: "time-for-trains.appspot.com",
-    messagingSenderId: "303628179740"
-  };
-  firebase.initializeApp(config);
+    // Time is 3:30 AM
+    var firstTime = "03:30";
 
-var database = firebase.database
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "minutes");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  
+
+//need code that updates the train list and submits the information to firebase //
+
+//you need to write the code that gets the train records from firebase
+
+ //review .push() method. 
+
+ //review database.ref().set()
